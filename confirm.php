a529b427532993aeb912f37deb15181aa580a1c3
KeitaroOtsuka <ktrotsuka@gmail.com>
2023-01-31 11:44:28 +0900

    first commit

---
+++ confirm.php
@@ -0,0 +1,155 @@
+<?php
+namespace shopping;
+
+require_once dirname(__FILE__) . '/Bootstrap.class.php';
+
+use shopping\Bootstrap;
+use shopping\lib\PDODatabase;
+use shopping\lib\Session;
+use shopping\lib\Item;
+use shopping\lib\Common;
+
+$db = new PDODatabase(Bootstrap::DB_HOST, Bootstrap::DB_USER, Bootstrap::DB_PASS, Bootstrap::DB_NAME, Bootstrap::DB_TYPE);
+$ses = new Session($db);
+$itm = new Item($db);
+$common = new Common();
+
+// テンプレート指定
+$loader = new \Twig_Loader_Filesystem(Bootstrap::TEMPLATE_DIR);
+$twig = new \Twig_Environment($loader, [
+      'cache' => Bootstrap::CACHE_DIR
+]);
+
+// モード判定(どの画面から来たかの判断)
+// 登録画面から来た場合
+if (isset($_POST['confirm']) === true) {
+    $mode = 'confirm';
+}
+// 戻る場合
+if (isset($_POST['back']) === true) {
+    $mode = 'back';
+}
+// 登録完了
+if (isset($_POST['complete']) === true) {
+    $mode = 'complete';
+}
+// ボタンのモードよって処理をかえる
+switch ($mode) {
+    case 'confirm': // 新規登録
+                    // データを受け継ぐ
+                    // ↓この情報は入力には必要ない
+        unset($_POST['confirm']);
+    
+        $dataArr = $_POST;
+        $dataArr['image'] = $_FILES['image'];
+
+        // この値を入れないでPOSTするとUndefinedとなるので未定義の場合は空白状態としてセットしておく
+        if (isset($_POST['ctg_id']) === false) {
+            $dataArr['ctg_id'] = "";
+        }
+
+        // エラーメッセージの配列作成
+        $commonArr = $common->errorCheck($dataArr);
+        $err_check = $common->getErrorFlg();
+
+        $errArr = $commonArr[0];
+        $image = $commonArr[1];
+
+        // err_check = false →エラーがありますよ！
+        // err_check = true →エラーがないですよ！
+        // エラー無ければconfirm.tpl あるとregist.tpl
+        if ($err_check === true) {
+            if (move_uploaded_file($image['tmp_name'], './images/' . $image['name']) === false) {
+                echo '画像のアップロードに失敗しました';
+                $errArr['image'] = '画像のアップロードに失敗しました';
+                $template = 'regist.html.twig';
+            } else {
+                $template = 'confirm.html.twig';
+            } 
+        } else {
+            $template = 'regist.html.twig';
+        }
+
+        break;
+
+    case 'back': // 戻ってきた時
+                 // ポストされたデータを元に戻すので、$dataArrにいれる
+        $dataArr = $_POST;
+
+        unset($dataArr['back']);
+
+        // エラーも定義しておかないと、Undefinedエラーがでる
+        foreach ($dataArr as $key => $value) {
+            $errArr[$key] = '';
+        }
+
+        $template = 'regist.html.twig';
+        break;
+
+    case 'complete': // 登録完了
+        $itemData = $_POST;
+        $image = $_POST['image'];
+        $itemData['image'] = $image[0];
+
+        // var_dump($_FILES);
+        // exit;
+
+        // var_dump($image);
+        // echo '<br>';
+        // echo $image[2], './images/' . $image[0];
+        // exit;
+        // if (move_uploaded_file($image[2], './images/' . $image[0]) === false) {
+        //     echo '失敗';
+        //     exit;
+        // }
+
+        // /Applications/MAMP/tmp/php/php5ewkH1./upload_1674069605.jpg
+        // /Applications/MAMP/tmp/php/phpLxejBe./images/jagaimo.jpeg
+
+        // ↓この情報はいらないので外しておく
+        unset($itemData['complete']);
+        // $column = '';
+        // $insData = '';
+
+        // // foreachの中でSQL文を作る
+        // foreach ($dataArr as $key => $value) {
+        //     $column .= $key . ', ';
+        //     // if ($key === 'traffic') {
+        //     //     $value = implode('_', $value);
+        //     // }
+        //     // $insData .= ($key === 'sex') ? $db->quote($value) . ',' : $db->str_quote($value) . ', ';
+        // }
+
+        $res = $itm->insItemData($itemData);
+
+        if ($res === true) {
+            // 登録成功時は完成ページへ
+            header('Location: ' . Bootstrap::ENTRY_URL . 'complete.php');
+            exit();
+        } else {
+            // 登録失敗時は登録画面に戻る
+            $template = 'regist.html.twig';
+
+            foreach ($dataArr as $key => $value) {
+                $errArr[$key] = '';
+            }
+        }
+
+        break;
+}
+
+$i = 1;
+foreach ($itm->getCategoryList() as $key => $val) {
+    $ctgArr[$i] = $val['category_name'];
+    $i ++;
+}
+
+$dataArr;
+
+$context = [];
+$context['dataArr'] = $dataArr;
+$context['errArr'] = $errArr;
+$context['ctgArr'] = $ctgArr;
+
+$template = $twig->loadTemplate($template);
+$template->display($context);
---
+++ lib/Common.class.php
@@ -0,0 +1,101 @@
+<?php
+namespace shopping\lib;
+
+class Common
+{
+    private $dataArr = [];
+
+    private $errArr = [];
+
+    public function errorCheck($dataArr)
+    {
+        $this->dataArr = $dataArr;
+        // クラス内のメソッドを読み込む
+        $this->createErrorMessage();
+
+        $this->itemNameCheck();
+        $this->ctgCheck();
+        $this->priceCheck();
+        $this->detailCheck();
+
+        $image = $this->imageCheck();
+
+        return [$this->errArr, $image];
+    }
+
+    private function createErrorMessage()
+    {
+        foreach ($this->dataArr as $key => $val) {
+            $this->errArr[$key] = '';
+        }
+    }
+
+    private function itemNameCheck()
+    {
+        if ($this->dataArr['item_name'] === '') {
+            $this->errArr['item_name'] = '商品名を入力してください';
+        }
+    }
+
+    private function ctgCheck()
+    {
+        if ($this->dataArr['ctg_id'] === '') {
+            $this->errArr['ctg_id'] = 'カテゴリーを選択してください';
+        }
+    }
+
+    private function priceCheck()
+    {
+        if (preg_match('/^\d{1,6}$/', $this->dataArr['price']) === 0 ||
+            strlen($this->dataArr['price']) >= 12) {
+
+            $this->errArr['tel1'] = '金額は、半角数字で11桁以内で入力してください';
+
+        }
+    }
+
+    private function detailCheck()
+    {
+        if ($this->dataArr['detail'] === '') {
+            $this->errArr['detail'] = '商品説明を入力してください';
+        }
+    }
+
+    private function imageCheck()
+    {
+        $image = $this->dataArr['image'];
+
+        if ($image === '') {
+            $this->errArr['image'] = '画像をアップロードしてください';
+        } else {
+            if ($image['error'] === 0 && $image['size'] !== 0) {
+                if (is_uploaded_file($image['tmp_name']) === true) {
+                    $image_info = getimagesize($image['tmp_name']);
+                    $image_mime = $image_info['mime'];
+                    if ($image['size'] > 1048576) {
+                        $this->errArr['image'] = 'アップロードできる画像のサイズは、1MBまでです';
+                    } elseif (preg_match('/^image\/jpeg$/', $image_mime) === 0) {
+                        $this->errArr['image'] = 'アップロードできる画像の形式は、JPEG形式(jpg/jpe/jpeg)だけです';
+                    } else {
+                        return $image;
+                    }
+                } else {
+                    $this->errArr['image'] = '正しくアップロードされませんでした';
+                }
+            } else {
+                $this->errArr['image'] = 'エラーが発生しました';
+            }
+        }
+    }
+
+    public function getErrorFlg()
+    {
+        $err_check = true;
+        foreach ($this->errArr as $key => $value) {
+            if ($value !== '') {
+                $err_check = false;
+            }
+        }
+        return $err_check;
+    }
+}
