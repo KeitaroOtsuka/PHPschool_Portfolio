3b79a8a708359a18d97c29164dfa80bbe73443b2
KeitaroOtsuka <ktrotsuka@gmail.com>
2023-02-07 14:28:04 +0900

    reCAPTCHA v3の導入完了

--- shopping/login.php
+++ shopping/login.php
@@ -19,6 +19,19 @@ if (isset($_POST['mail']) === true && isset($_POST['pass']) === true)
     $mail = $_POST['mail'];
     $pass = $_POST['pass'];
     list($msg, $link) = $db->checkLogin($mail,$pass);
+    // reCAPTCHA v3にてBOT判定
+    if (isset($_POST["recaptchaResponse"]) && !empty($_POST["recaptchaResponse"])) {
+        $secret = "6LdneFwkAAAAAHLz-zmbY2p6ePLntLkoKySLM1Ei";
+        $verifyResponse = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=".$secret."&response=".$_POST["recaptchaResponse"]);
+        $reCAPTCHA = json_decode($verifyResponse);
+        if ($reCAPTCHA->success) {
+            echo "認証成功";
+        } else {
+            echo "認証エラー";
+        }
+    } else {
+        echo "不正アクセス";
+    }    
 }
 // var_dump($msg);
 ?>
--- shopping/login_form.php
+++ shopping/login_form.php
@@ -9,6 +9,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>tc</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <script src="https://www.google.com/recaptcha/api.js?render=6LdneFwkAAAAAF-awKK13QDXVgog_mKGoPuJnJyR"></script>
+    <script>
+    grecaptcha.ready(function () {
+        grecaptcha.execute("6LdneFwkAAAAAF-awKK13QDXVgog_mKGoPuJnJyR", {action: "sent"}).then(function(token) {
+        var recaptchaResponse = document.getElementById("recaptchaResponse");
+        recaptchaResponse.value = token;
+        });
+    });
+    </script>
   </head>
   <body class="text-center">
     <h1>ログインページ</h1>
@@ -25,6 +34,7 @@
             <input type="password" name="pass" required>
         </label>
     </div>
+    <input type="hidden" name="recaptchaResponse" id="recaptchaResponse">
     <input type="submit" value="ログイン">
     </form>
   </body>
